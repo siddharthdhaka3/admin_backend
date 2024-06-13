c623cb05d24851d4528dbe1980a54d1b284b7673
siddharth <siddharthdhaka03@gmail.com>
2024-06-13 18:00:57 +0530

    feedbacks

--- src/apps/helper/response.ts
+++ src/apps/helper/response.ts
@@ -1,25 +1,25 @@
-// interface IResponse {
-//     success: boolean;
-//     message?: string;
-//     data: object | null | any;
-//   }
+interface IResponse {
+    success: boolean;
+    message?: string;
+    data: object | null | any;
+  }
   
-//   export type ErrorResponse = IResponse & {
-//     error_code: number;
-//   };
-  
-//   // export const createResponse = (
-//   //   data: IResponse["data"],
-//   //   message?: string
-//   // ): IResponse => {
-//   //   return { data, message, success: true };
-//   // };
+  export type ErrorResponse = IResponse & {
+    error_code: number;
+  };
   
   export const createResponse = (
-    data: object | null | any,
+    data: IResponse["data"],
     message?: string
-  ): any => {
-    const response = { data, message, success: true };
-    return response;
+  ): IResponse => {
+    return { data, message, success: true };
   };
+  
+  // export const createResponse = (
+  //   data: object | null | any,
+  //   message?: string
+  // ): any => {
+  //   const response = { data, message, success: true };
+  //   return response;
+  // };
   
